5eb8a7b75ea0660424ef80d54fbd38cacf82df74
Bo Dai <daibo@infini-ai.com>
2025-08-26 11:01:56 +0000

    feat(vllm): implement vllm worker
    Signed-off-by: Bo Dai <daibo@infini-ai.com>

--- rlinf/hybrid_engines/vllm/vllm_0_7_1/worker.py
+++ rlinf/hybrid_engines/vllm/vllm_0_7_1/worker.py
@@ -1,13 +1,82 @@
-from vllm.v1.worker.gpu_worker import Worker as _Worker
-from vllm.config import VllmConfig
+# Copyright 2025 The RLinf Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-class VLLMWorker(_Worker):
-    def __init__(self, 
-                 vllm_config: VllmConfig, 
-                 local_rank: int,
-                 rank: int, 
-                 distrubuted_init_method: str,
-                 is_driver_worker: bool = False):
-        super().__init__(vllm_config, local_rank, rank, distrubuted_init_method, is_driver_worker)
-    
-    
+from omegaconf import DictConfig
+from vllm.config import VllmConfig
+from vllm.v1.worker.gpu_worker import Worker as _VllmInnerWorker
+
+from rlinf.scheduler import Worker as _RLinfWorker
+from rlinf.scheduler import WorkerAddress
+from rlinf.utils.placement import ModelParallelComponentPlacement, PlacementMode
+from rlinf.workers.rollout.utils import DisaggRankMapper, HybridRankMapper
+
+
+class VLLMWorker(_VllmInnerWorker, _RLinfWorker):
+    def __init__(
+        self,
+        vllm_config: VllmConfig,
+        rlinf_config: DictConfig,
+        distributed_init_method: str,
+        parent_address: WorkerAddress,
+        placement: ModelParallelComponentPlacement,
+        local_rank: int,
+        rank: int,
+        world_size: int,
+    ):
+        _RLinfWorker.__init__(
+            self, parent_address=parent_address, world_size=world_size, rank=rank
+        )
+        super().__init__(vllm_config, local_rank, rank, distrubuted_init_method)
+
+        self.rlinf_config = rlinf_config
+        self._actor_group_name = self.rlinf_config.actor.group_name
+        self.placement_mode = placement.placement_mode
+        if self.placement_mode == PlacementMode.COLLOCATED:
+            self.actor_weight_rank = (
+                HybridRankMapper.get_rollout_rank_to_actor_rank_map(
+                    self.cfg.actor.model.tensor_model_parallel_size,
+                    self.cfg.actor.model.pipeline_model_parallel_size,
+                    self.cfg.rollout.tensor_parallel_size,
+                    self.cfg.cluster.num_nodes * self.cfg.cluster.num_gpus_per_node,
+                )[self.get_parent_rank(), self._rank]
+            )
+        elif self.placement_mode == PlacementMode.DISAGGREGATED:
+            rank_map = DisaggRankMapper.get_rollout_rank_to_actor_rank_map(
+                actor_tp_size=self.cfg.actor.model.tensor_model_parallel_size,
+                actor_pp_size=self.cfg.actor.model.pipeline_model_parallel_size,
+                actor_world_size=placement.actor_world_size,
+                rollout_tp_size=self.cfg.rollout.tensor_parallel_size,
+                rollout_world_size=placement.rollout_world_size,
+            )
+            self.log_info(
+                f"Rollout rank to actor rank mapping: {rank_map}, try to get {(self.get_parent_rank(), self._rank)}"
+            )
+            self.actor_weight_rank = rank_map[self.get_parent_rank(), self._rank]
+        else:
+            raise ValueError(f"Unsupported placement mode: {self.placement_mode}")
+
+        self.log_info(
+            f"Running VllmInnerWoker dp rank {self.get_parent_rank()}, tp_rank {rank}, corresponding actor weight rank = {self.actor_weight_rank}"
+        )
+
+    def _dispatch_loop(self):
+        raise NotImplementedError("VLLMWorker._dispatch_loop is not implemented yet.")
+
+    def sync_weight(self):
+        raise NotImplementedError("VLLMWorker.sync_weight is not implemented yet.")
+
+    def offload_model_weights(self):
+        raise NotImplementedError(
+            "VLLMWorker.offload_model_weights is not implemented yet."
+        )
--- rlinf/workers/rollout/vllm/vllm_worker.py
+++ rlinf/workers/rollout/vllm/vllm_worker.py
@@ -1,14 +1,32 @@
+# Copyright 2025 The RLinf Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from omegaconf import DictConfig
-from rlinf.scheduler import Channel, Cluster, Worker
+from vllm.config import VllmConfig
+from vllm.engine.arg_utils import EngineArgs
+from vllm.sampling_params import SamplingParams
+
+from rlinf.config import torch_dtype_from_precision
+from rlinf.data.io_struct import RolloutRequest
+from rlinf.scheduler import Channel, Worker
 from rlinf.utils.placement import ComponentPlacement
-from rlinf.data.io_struct import (
-    RolloutRequest,
-    RolloutResult
-)
+
+from . import VLLMEngine
 
 
 class VLLMWorker(Worker):
-
     def __init__(self, config: DictConfig, placement: ComponentPlacement):
         Worker.__init__(self)
         self._cfg = config
@@ -16,6 +34,9 @@ class VLLMWorker(Worker):
 
         self._return_logprobs = self._cfg.rollout.return_logprobs
         self._sampling_params = self._get_sampling_params_from_config()
+
+        self._vllm_engine = None
+
         if self._cfg.algorithm.rollout_batch_size_per_gpu is None:
             self._rollout_batch_size = None
         else:
@@ -24,22 +45,69 @@ class VLLMWorker(Worker):
                 * self._cfg.rollout.tensor_parallel_size
                 * self._cfg.rollout.pipeline_parallel_size
             )
-        
-    def _get_sampling_params_from_config(self) -> dict:
-        raise NotImplementedError("VLLMWorker._get_sampling_params_from_config is not implemented yet.")
+
+    def _get_sampling_params_from_config(self) -> SamplingParams:
+        cfg_sampling_params = self._cfg.algorithm.sampling_params
+        if cfg_sampling_params.use_greedy:
+            sampling_params = SamplingParams(
+                temperature=0,
+                max_tokens=cfg_sampling_params.max_new_tokens,
+            )
+        else:
+            sampling_params = SamplingParams(
+                temperature=cfg_sampling_params.temperature,
+                top_k=cfg_sampling_params.top_k,
+                top_p=cfg_sampling_params.top_p,
+                repetition_penalty=cfg_sampling_params.repetition_penalty,
+                max_tokens=cfg_sampling_params.max_new_tokens,
+            )
+        return sampling_params
 
     def sync_model_from_actor(self) -> None:
-        raise NotImplementedError("VLLMWorker.sync_model_from_actor is not implemented yet.")
-    
+        self._vllm_engine.sync_hf_weight()
+
     def init_worker(self) -> None:
-        raise NotImplementedError("VLLMWorker.init_worker is not implemented yet.")
+        engine_args: EngineArgs = EngineArgs(
+            model=self._cfg.rollout.model_dir,
+            tensor_parallel_size=self._cfg.rollout.tensor_parallel_size,
+            dtype=torch_dtype_from_precision(self._cfg.model.precision),
+            gpu_memory_utilization=self._cfg.rollout.gpu_memory_utilization,
+            enforce_eager=self._cfg.rollout.enforce_eager,
+        )
+        vllm_config: VllmConfig = engine_args.create_engine_config()
+
+        self._vllm_engine = VLLMEngine(
+            vllm_config=vllm_config,
+            log_stats=True,  # temporarily True for debug
+            multiprocess_model=True,  # use SyncMPClient
+        )
+
+        self._vllm_engine.offload_model_weights()
 
     def _stop(self) -> None:
-        raise NotImplementedError("VLLMWorker._stop is not implemented yet."):
+        self.log_debug(
+            f"[LLM dp {self._rank}] Received None input tokens, rollout end."
+        )
+        self._vllm_engine.offload_model_weights()
 
     def rollout(self, input_channel: Channel, output_channel: Channel) -> None:
         while True:
             request: RolloutRequest = input_channel.get()
             if request is None:
                 self._stop()
-                
+                break
+
+            requests = request.repeat_and_split(self._rollout_batch_size)
+            rollout_results = []
+            for request in requests:
+                results = self._vllm_engine.generate(
+                    input_ids=request.input_ids,
+                    sampling_params=self._sampling_params,
+                    return_logprobs=self._return_logprobs,
+                )
+                # should be converted by _vllm_engine side.
+                rollout_results.append(results)
+
+                if self._cfg.rollout.print_outputs:
+                    raise NotImplementedError("TODO")
+            output_channel.put(rollout_results)
