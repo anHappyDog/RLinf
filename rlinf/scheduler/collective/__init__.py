c5ef7fdba850c468cff46e387567e598525175f2
cc <daibo@infini-ai.com>
2026-08-12 04:04:19 +0000

    feat: support tensor compression in collective group
    Signed-off-by: cc <daibo@infini-ai.com>

--- rlinf/scheduler/collective/__init__.py
+++ rlinf/scheduler/collective/__init__.py
@@ -23,6 +23,7 @@ from .async_work import (
 )
 from .collective import Collective
 from .collective_group import CollectiveGroup, CollectiveGroupOptions
+from .tensor_compression import TensorCompressionOptions
 
 __all__ = [
     "AsyncWork",
@@ -35,4 +36,5 @@ __all__ = [
     "CollectiveGroup",
     "Collective",
     "CollectiveGroupOptions",
+    "TensorCompressionOptions",
 ]
